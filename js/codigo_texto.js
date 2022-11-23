5cd8d68356268309bfa15f2ff69d06d4eca7db00
Lorenzo MR <lorenzo.mr.rm@gmail.com>
2022-11-22 21:13:39 -0300

    Botão para subir ao topo(ex_eventos)

--- js/codigo_texto.js
+++ js/codigo_texto.js
@@ -15,3 +15,20 @@ for (let i = 0; i < cod.length; i++) {
 // &amp; = &
 // &lt; = <
 // &gt; = >
+
+let mybutton = document.getElementById("Btn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
